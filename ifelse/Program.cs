1264deb64146744bc2a48c6c74f6f8cf589c2533
Bilal Furkan Tozluyurt <61858039+bilaltozluyurt@users.noreply.github.com>
2025-02-27 04:07:13 +0300

    Add files via upload
    karar yapıları çalışması tamamlandı

---
+++ ifelse/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            if (time > 6 && time <= 12)
+            {
+                Console.WriteLine("Günaydın");
+            }
+            else if (time > 12 && time <= 18)
+            {
+                Console.WriteLine("İyi günler");
+            }
+            else
+            {
+                Console.WriteLine("İyi geceler");
+            }
+            Console.WriteLine(time > 6 && time <= 12 ? "Günaydın" : time > 12 && time <= 18 ? "İyi günler" : time > 18 ? "İyi geceler" : "İyi geceler"); 
+        }
+    }
+}
