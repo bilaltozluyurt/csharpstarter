fb3d5bdcc5d9c5ecc22400741804c404c73257a7
Bilal Furkan Tozluyurt <61858039+bilaltozluyurt@users.noreply.github.com>
2025-03-06 03:15:09 +0300

    Update Program.cs
    array metodu eklendi.

--- diziler/Program.cs
+++ diziler/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace diziler
 {
@@ -24,7 +24,7 @@ namespace diziler
             int[] sayidizi = new int[dizilength];
             for (int i = 0; i < sayidizi.Length; i++)
             {
-                Console.WriteLine("Diziye {0}. sayıyı giriniz :",i+1);
+                Console.WriteLine("Diziye {0}. sayıyı giriniz :", i + 1);
                 sayidizi[i] = int.Parse(Console.ReadLine());
 
             }
@@ -34,8 +34,54 @@ namespace diziler
                 toplam += sayi;
 
             }
-            Console.WriteLine("Ortalama :" +toplam/dizilength);
-             
+            Console.WriteLine("Ortalama :" + toplam / dizilength);
+
+            Console.WriteLine("UNSORTED");
+            int[] ndizi = { 14, 25, 88, 91, 63, 28, 42, 9, 50 };
+            foreach (var sayi in ndizi)
+                Console.WriteLine(sayi);
+            //SORTED
+            Console.WriteLine("SORTED");
+            Array.Sort(ndizi);
+            foreach (var sayi in ndizi)
+                Console.WriteLine(sayi);
+            //REVERSE   
+            Console.WriteLine("REVERSE");
+            Array.Reverse(ndizi);
+            foreach (var sayi in ndizi)
+                Console.WriteLine(sayi);
+            //CLEAR
+            Console.WriteLine("CLEAR");
+            Console.WriteLine("array.clear fonksiyonu test");
+            Array.Clear(ndizi, 2, 3);
+            foreach (var sayi in ndizi)
+                Console.WriteLine(sayi);
+            //indexof  
+            Console.WriteLine("INDEXOF");
+            // dizide 9 elemanı kaçıncı indexte
+            Console.WriteLine(Array.IndexOf(ndizi, 9));
+            //dizide olmayan eleman arayalım
+            Console.WriteLine(Array.IndexOf(ndizi, 100));
+            //aradığımız eleman dizide yoksa -1 değeri dönüyor.
+
+            //RESIZE 
+            int[] boyut = new int[10];
+            Array.Resize<int>(ref boyut, 101);
+            Random rn = new Random();
+            for (int i = 0; i < boyut.Length; i++)
+            {
+
+                int deger=rn.Next(0, 200);
+                boyut[i] = deger;
+            }
+            for (int j = 0; j < boyut.Length; j++)
+            {
+                Console.WriteLine(boyut[j]);
+            }
+            
+            Array.Resize<int>(ref boyut,101);
+
+            
 
         }
     }
